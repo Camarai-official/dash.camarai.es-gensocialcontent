d5b34bd5ffb597393e58ce51fc6ab62dfacc526d
Keny Camarai <society@camarai.es>
2025-10-01 18:26:03 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// Archivo de configuración
+const CONFIG = {
+    // Accede a la variable de entorno de GitHub a través de process.env
+    API_URL: process.env.URL_API
+};
+
+// Exportar para uso en otros archivos
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+} else {
+    window.CONFIG = CONFIG;
+}
